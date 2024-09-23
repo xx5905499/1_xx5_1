aeef40faccbc475ba73b81e60feefb26c98d1f73
TTV <notestmn905499@gmail.com>
2024-09-23 08:55:52 -0400

    Update contributors.js

--- contributors.js
+++ contributors.js
@@ -1,23 +1,23 @@
 let table = document.getElementById("allScores");
 
-function renderdescriptionTable(description) {
+function rendercontributionsTable(contributions) {
   let i = 1;
-  description.forEach(({ login, html_url, description }) => {
+  contributions.forEach(({ login, html_url, contributions }) => {
     let row = table.insertRow(i);
     let slNoCell = row.insertCell(0);
     let userNameCell = row.insertCell(1);
     let urlUser = row.insertCell(2);
-    let noOfdescription = row.insertCell(3);
+    let noOfcontributions = row.insertCell(3);
     slNoCell.innerText = i;
     userNameCell.innerText = login;
     urlUser.innerHTML = `<a href="${html_url}">${html_url}</a>`;
-    noOfdescription.innerText = description;
+    noOfcontributions.innerText = contributions;
     i++;
   });
-  document.getElementById("DI").innerHTML = i - 1 + " description";
+  document.getElementById("ct").innerHTML = i - 1 + " contributions";
 }
 
-fetch("https://api.kirka.io/api/clans/Reverie")
+fetch("https://api.github.com/repos/xx5905499/1_xx5_1/contributors")
   .then((response) => response.json())
-  .then((description) => renderdescriptionTable(description))
+  .then((contributions) => rendercontributionsTable(contributions))
   .catch((err) => console.log("Request Failed", err));
