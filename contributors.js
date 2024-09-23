fabb8a2410ad46802e51fc7f924ffe1c879810b9
TTV <notestmn905499@gmail.com>
2024-09-23 08:50:31 -0400

    Update contributors.js

--- contributors.js
+++ contributors.js
@@ -1,17 +1,17 @@
 let table = document.getElementById("counthere");
 
-function renderContributorsTable(contributors) {
+function renderdescriptionTable(description) {
   let i = 1;
-  contributors.forEach(({ login, html_url, description }) => {
+  description.forEach(({ login, html_url, description }) => {
     let row = table.insertRow(i);
     let slNoCell = row.insertCell(0);
     let userNameCell = row.insertCell(1);
     let urlUser = row.insertCell(2);
-    let noOfContributions = row.insertCell(3);
+    let noOfdescription = row.insertCell(3);
     slNoCell.innerText = i;
     userNameCell.innerText = login;
     urlUser.innerHTML = `<a href="${html_url}">${html_url}</a>`;
-    noOfContributions.innerText = contributions;
+    noOfdescription.innerText = description;
     i++;
   });
   document.getElementById("DI").innerHTML = i - 1 + " description";
@@ -19,5 +19,5 @@ function renderContributorsTable(contributors) {
 
 fetch("https://api.kirka.io/api/clans/Reverie")
   .then((response) => response.json())
-  .then((contributors) => renderContributorsTable(description))
+  .then((description) => renderdescriptionTable(description))
   .catch((err) => console.log("Request Failed", err));
